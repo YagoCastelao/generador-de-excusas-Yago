f7354c3cdb4c58f597f40bb27ac7272a4ebe1b21
Yago Cima Castelao <yago_castelau@hotmail.com>
2024-09-10 10:37:17 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#btn").addEventListener("click", function() {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = function() {
+  let pronoun = ["Una", "Uno", "El", "La"];
+  let subject = ["motorista", "perro", "hermano", "gato", "tortuga", "vecino"];
+  let action = ["tomó mi", "tiré mi", "me gritó", "me robó mi", "me mordió"];
+  let possetion = ["tarea", "dedo del pie", "coche", "zapato"];
+  let where = ["en la calle", "en mi casa", "en mi camino de entrada"];
+
+  let proindx = Math.floor(Math.random() * pronoun.length);
+  let subjtindx = Math.floor(Math.random() * subject.length);
+  let actindx = Math.floor(Math.random() * action.length);
+  let posindx = Math.floor(Math.random() * possetion.length);
+  let wheindx = Math.floor(Math.random() * where.length);
+  return (
+    pronoun[proindx] +
+    " " +
+    subject[subjtindx] +
+    " " +
+    action[actindx] +
+    " " +
+    possetion[posindx] +
+    " " +
+    where[wheindx]
+  );
 };
